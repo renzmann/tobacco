7f5e2c7d0d4ed0f5aac207b6b92dece3e75a4a6a
Robb Enzmann <robbenzmann@gmail.com>
2021-04-01 15:19:19 -0400

    add build and license info

--- setup.py
+++ setup.py
@@ -4,12 +4,23 @@
 #  Unauthorized copying of this file, via any medium, is strictly prohibited.
 #  --------------------------------------------------------------------------
 
+import pathlib
+
 from setuptools import setup, find_packages
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 
 setup(
     name="tobacco",
-    version="1.0.0",
+    version="1.0.1",
     description="Function pipelines for python",
     author="Robert Enzmann",
+    url="https://github.com/renzmann/tobacco",
+    long_description=README,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
+    license="MIT",
 )
