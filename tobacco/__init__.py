d9309b63223afc676a6410e05739e907e591ef4e
Robert Enzmann <32076780+renzmann@users.noreply.github.com>
2021-05-21 16:46:26 -0400

    whoops on copyright

--- tobacco/__init__.py
+++ tobacco/__init__.py
@@ -1,9 +1,3 @@
-#  --------------------------------------------------------------------------
-#  Copyright (c) 2021, antuit.ai. All rights reserved.
-#  Proprietary and confidential
-#  Unauthorized copying of this file, via any medium, is strictly prohibited.
-#  --------------------------------------------------------------------------
-
 from ._pype import pipe
 
 __version__ = "1.0.3"
