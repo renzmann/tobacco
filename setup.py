f6f67adb56b39991dd7c2041b11849459c56c8be
Robert Enzmann <robert.enzmann@antuit.com>
2021-06-27 17:36:12 -0400

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tobacco",
-    version="1.0.3",
+    version="1.0.4",
     description="Function pipelines for python",
     author="Robert Enzmann",
     url="https://github.com/renzmann/tobacco",
--- tobacco/__init__.py
+++ tobacco/__init__.py
@@ -1,4 +1,4 @@
 from ._pype import pipe
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
